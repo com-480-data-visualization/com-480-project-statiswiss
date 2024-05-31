59ab84978239fa7186c9f927dd82fde4d4365b16
Nicolapps <nicolas.ettlin@me.com>
2024-05-31 09:11:13 +0000

    Deploying to gh-pages from @ com-480-data-visualization/com-480-project-statiswiss@8f624b82b030cd2e5b209b3d979c3c194b42a659 🚀

---
+++ resources/search.js
@@ -0,0 +1,54 @@
+(() => {
+  function debounce(func, ms = 100) {
+    let timeout;
+    return function() {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(this, arguments), ms);
+    };
+  }
+    
+  const search = document.querySelector('.js-search');
+  search.placeholder = {"en": "Search for a vote", "fr": "Chercher une votation", "de": "Eine Abstimmung suchen"}[lang];
+
+  /** @type {import("convex/browser")["ConvexClient"]} */
+  const ConvexClient = convex.ConvexClient;
+  const client = new ConvexClient('https://artful-minnow-484.convex.cloud');
+  /** @type {import("../../convex/_generated/api")["api"]} */
+  const api = convex.anyApi;
+
+  const listAllResults = document.querySelector('#list_refs');
+  const listSearchResults = document.querySelector('.js-search-results');
+  const loader = document.querySelector('.js-search-loader');
+
+  const doQuery = debounce(async (query) => {
+    console.log('searching', query)
+    const results = await client.action(api.search.default, { query });
+
+    if (query !== search.value.trim()) return;
+    console.log(results)
+
+    let html = '';
+    for (const row of results) {
+      html += showVote(row, lang);
+    }
+
+    loader.style.display = 'none';
+    listSearchResults.innerHTML = html;
+  });
+
+  search.addEventListener('input', async () => {
+    listSearchResults.innerHTML = '';
+    const query = search.value.trim();
+    if (query === '') {
+      listAllResults.style.display = 'block';
+      loader.style.display = 'none';
+      return;
+    }
+
+    listAllResults.style.display = 'none';
+    loader.style.display = 'block';
+
+    doQuery(query);
+  });
+
+})();
